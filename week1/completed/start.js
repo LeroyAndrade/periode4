069c4a2f0140c4b4e9ac1c8aad2ddc5ba04c1250
s.hitipeuw <werksimon@gmail.com>
2020-05-06 14:47:09 +0200

    Readme

---
+++ week1/completed/start.js
@@ -0,0 +1,85 @@
+const bmi = document.getElementById("bmi");
+const randomGetal = document.getElementById("randomgetal");
+const baseUrl = "bmi.php";
+
+randomGetal.innerText = "Random getal: " + Math.floor(Math.random()* 10);
+
+form.addEventListener('submit', function (event) {
+
+    // TODO: maak de functie af en
+    //  haal de gegevens op voor gewicht en lengte uit het formulier.
+
+    const gewicht = document.forms["form"]["gewicht"].value;
+    const lengte = document.forms["form"]["lengte"].value;
+
+    const url = createURL(baseUrl, gewicht, lengte);
+
+    // TODO: lees de onderstaande url
+    //  en beschrijf in eigen woorden in je readme.md waar de event.preventDefault()
+    //  voor dient.
+    //  https://developer.mozilla.org/en-US/docs/Web/API/HTMLFormElement/submit_event
+
+    getRequest(url, callback);
+
+    // TODO: Wat gebeurt er als je de preventDefault() weg laat.
+    event.preventDefault();
+
+});
+
+function createURL(url, gewicht, lengte) {
+    // TODO: maak de functie af zodat
+    //  de return door de php pagina verwerkt kan worden
+    //  voorbeeld: index.php?naam=voornaam&achternaam=achternaam
+
+    return url + "?" +
+        "gewicht=" + gewicht +
+        "&" +
+        "lengte=" + lengte;
+}
+
+
+function callback(response) {
+    // TODO: Maak de functie af zodat je een goed advies krijgt over je BMI
+    //  maak gebruik van een switch hiervoor
+    //  Meer info: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/switch
+    //  < 18.5 is ondergewicht
+    //  18.5 - 24.9 is normaal gewicht
+    //  25 - 29.9 is overgewicht
+    //  >= 30 is obesitas
+    bmi.innerText = response;
+
+    // TODO: Extra punten te verdienen
+    //  Het BMI is afhankelijk van leeftijd en geslacht
+    //  Maak een advies op basis van leeftijd en geslacht
+    //  Meer info: https://www.voedingscentrum.nl/nl/bmi-meter.aspx
+}
+
+function getRequest(url, callBack) {
+
+    let request = new XMLHttpRequest()
+
+    request.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+
+            // TODO: Lees de onderstaande URL's en beschrijf in eigen
+            //  woorden in je readme.md wat een callback functie is.
+            //  https://developer.mozilla.org/en-US/docs/Glossary/Callback_function
+            //  https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Asynchronous/Introducing
+
+            // TODO: Beschrijf in eigen woorden in je readme.md wat het verschil is tussen
+            //  een asynchrone en een synchrone callback functie.
+
+            let response = request.responseText;
+            callBack(response)
+
+        }
+    };
+
+    request.open(
+        'GET',
+        url,
+        true
+    );
+
+    request.send();
+}
---
+++ week1/readme.md
@@ -0,0 +1,24 @@
+# AJAX
+##  Asynchronous JavaScript And XML
+
+Het is een techniek om gegevens van een webservers te bereiken vanuit een webpagina.
+
+## Wat is belangrijk bij AJAX
+
+- XmlHttpRequest (Browser object)
+- JavaScript, HTML DOM om de gegevens in de browser te laten zien.
+
+Hoe AJAX werkt kun je mooi zien op W3Schools [Overzicht](https://www.w3schools.com/xml/ajax_intro.asp) 
+
+## Wat is nu Asynchronous
+Opdrachten in JavaScript worden normaal gesproken achter elkaar uitgevoerd, zodra de opdracht is afgerond wordt de volgende uitgevoerd.   
+
+Dat is niet in alle gevallen handig of gewenst, een proces dat erg lang duurt zoals het ophalen van een grote afbeelding vanaf een webserver laat je programma foutlopen. Je kunt namelijk geen afbeelding tonen die er nog niet is.
+
+
+Asynchronous uitvoeren van een opdracht heeft dit probleem niet, je programma gaat gewoon verder en zodra je bestand is gedownload krijg je een seintje dat de download gereed is.
+
+## Waarom XML
+XML was in het verleden de standaard om gegevens uit te wisselen. Tegenwoordig is het veel gebruikelijker om gegevens met JSON uit te wisselen. 
+
+Je kunt nu kiezen in welk formaat je gegevens terugwilt krijgen. 
